fabf50af00cbccc4777486019fc56315752e52c3
thmsn <9084377+thmsndk@users.noreply.github.com>
2022-07-17 02:37:35 +0200

    only use tooltip

--- InventoryTweaks/Patches/Patch_PlayerInventory_RemoveStacksFromSlot_.cs
+++ InventoryTweaks/Patches/Patch_PlayerInventory_RemoveStacksFromSlot_.cs
@@ -1,58 +1,58 @@
-﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿//using HarmonyLib;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using UnityEngine;
 
-namespace thmsn.InventoryTweaks.Patches
-{
-    /// <summary>
-    /// Replaces item with already existing item from inventory.
-    /// </summary>
-    [HarmonyPatch(typeof(PlayerInventory), "RemoveStacksFromSlot", typeof(Slot), typeof(int))]
-    class Patch_PlayerInventory_RemoveStacksFromSlot
-    {
-        // TODO: there seems to be an issue on death with duplication, either that, or when picking up really broken items.
+//namespace thmsn.InventoryTweaks.Patches
+//{
+//    /// <summary>
+//    /// Replaces item with already existing item from inventory.
+//    /// </summary>
+//    [HarmonyPatch(typeof(PlayerInventory), "RemoveStacksFromSlot", typeof(Slot), typeof(int))]
+//    class Patch_PlayerInventory_RemoveStacksFromSlot
+//    {
+//        // TODO: there seems to be an issue on death with duplication, either that, or when picking up really broken items.
 
-        static void Prefix(PlayerInventory __instance, Slot slot, int stacksToRemove, out HotBarSlotAndItemInstance __state)
-        {
-            //Debug.Log("RemoveStacksFromSlot");
-            __state = new HotBarSlotAndItemInstance
-            {
-                HotBarSlot = slot,
-                ItemInstance = slot.itemInstance
-            };
-            //csrun RAPI.GetLocalPlayer().Inventory.GetSelectedHotbarSlot().itemInstance.Uses = 1;
-            //Debug.Log($"{slot.itemInstance.UniqueName} uses {slot.itemInstance.Uses} / {slot.itemInstance.BaseItemMaxUses}");
-        }
+//        static void Prefix(PlayerInventory __instance, Slot slot, int stacksToRemove, out HotBarSlotAndItemInstance __state)
+//        {
+//            //Debug.Log("RemoveStacksFromSlot");
+//            __state = new HotBarSlotAndItemInstance
+//            {
+//                HotBarSlot = slot,
+//                ItemInstance = slot.itemInstance
+//            };
+//            //csrun RAPI.GetLocalPlayer().Inventory.GetSelectedHotbarSlot().itemInstance.Uses = 1;
+//            //Debug.Log($"{slot.itemInstance.UniqueName} uses {slot.itemInstance.Uses} / {slot.itemInstance.BaseItemMaxUses}");
+//        }
 
-        static void Postfix(PlayerInventory __instance, int stacksToRemove, HotBarSlotAndItemInstance __state, bool __result)
-        {
-            if (__result)
-            {
-                // item broke
-                var player = RAPI.GetLocalPlayer();
-                var selectedHotBarSlot = __state.HotBarSlot;
-                var itemInstance = __state.ItemInstance;
+//        static void Postfix(PlayerInventory __instance, int stacksToRemove, HotBarSlotAndItemInstance __state, bool __result)
+//        {
+//            if (__result)
+//            {
+//                // item broke
+//                var player = RAPI.GetLocalPlayer();
+//                var selectedHotBarSlot = __state.HotBarSlot;
+//                var itemInstance = __state.ItemInstance;
                 
-                foreach (Slot slot in player.Inventory.allSlots)
-                {
-                    if (slot.itemInstance != null && slot.itemInstance.UniqueName == itemInstance.UniqueName && slot != selectedHotBarSlot)
-                    {
-                        selectedHotBarSlot.SetItem(slot.itemInstance); // Assign the item from the slot to the hotbar;
-                        slot.Reset(); // Clear the slot of the item;
-                        break;
-                    }
-                }
-            }
-        }
+//                foreach (Slot slot in player.Inventory.allSlots)
+//                {
+//                    if (slot.itemInstance != null && slot.itemInstance.UniqueName == itemInstance.UniqueName && slot != selectedHotBarSlot)
+//                    {
+//                        selectedHotBarSlot.SetItem(slot.itemInstance); // Assign the item from the slot to the hotbar;
+//                        slot.Reset(); // Clear the slot of the item;
+//                        break;
+//                    }
+//                }
+//            }
+//        }
 
-        private class HotBarSlotAndItemInstance
-        {
-            public ItemInstance ItemInstance { get; set; }
-            public Slot HotBarSlot { get; set; }
-        }
-    }
-}
+//        private class HotBarSlotAndItemInstance
+//        {
+//            public ItemInstance ItemInstance { get; set; }
+//            public Slot HotBarSlot { get; set; }
+//        }
+//    }
+//}
